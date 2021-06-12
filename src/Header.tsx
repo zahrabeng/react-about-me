a09d8f612da1169e2a41cf5ef496701bf52b3d4e
Richard Ng <richardcrng@gmail.com>
2021-06-12 21:13:16 +0100

    Add Header component

---
+++ src/Header.tsx
@@ -0,0 +1,15 @@
+
+function Header(): JSX.Element {
+  return (
+    <header>
+      <h1>Ye Olde Web Page</h1>
+      <p>Welcome to Nichard's Ye Olde Web Page!</p>
+      <p>
+        This shows a random scatter of my thoughts, last updated on March 4,
+        2003.
+      </p>
+    </header>
+  );
+}
+
+export default Header
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import ReactDOM from "react-dom";
+import Header from "./Header";
 import MainContent from "./MainContent";
 
+ReactDOM.render(<Header />, document.getElementById("page-header"));
 ReactDOM.render(<MainContent />, document.getElementById("main-content"));
