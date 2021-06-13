fd83d1b5bd18c85e654ae68090991525cb250758
Richard Ng <richardcrng@gmail.com>
2021-06-13 16:29:29 +0100

    Create PageFooter component

---
+++ src/components/PageFooter.tsx
@@ -0,0 +1,9 @@
+function PageFooter(): JSX.Element {
+  return (
+    <footer>
+      <p>SITE CONTENTS NOT-REALLY-COPYRIGHT-BUT-WHATEVER, 2003, NICHARD RG</p>
+    </footer>
+  );
+}
+
+export default PageFooter
